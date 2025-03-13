86bff20586a8ff950b4193ca6abff95a5e8dd228
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-03-13 14:15:07 +0100

    NEW TodoItem component

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,3 +1,5 @@
+import TodoItem from "./TodoItem";
+
 export function TodoApp() {
   return (
     <>
@@ -10,39 +12,9 @@ export function TodoApp() {
       </div>
 
       <div className="my-5 flex-column gap-5 w-full text-left">
-        {/* TODO ITEM version normal */}
-        <div className="bg-indigo-700 w-full m-5 rounded-box p-3 flex">
-          <span className="pr-8">
-            <input type="checkbox" className="checkbox" />
-          </span>
-          <span className="flex-grow">Acheter des oranges</span>
-          <div>
-            <button className="btn btn-error btn-outline btn-xs">X</button>
-          </div>
-        </div>
-
-        {/* TODO Item version cochée */}
-        <div className="bg-indigo-900 w-full m-5 rounded-box p-3 flex">
-          <span className="pr-8">
-            <input type="checkbox" checked={true} className="checkbox" />
-          </span>
-          <span className="flex-grow line-through">Courir avec le fraté</span>
-          <div>
-            <button className="btn btn-error btn-outline btn-xs">X</button>
-          </div>
-        </div>
-
-        <div className="bg-indigo-900 w-full m-5 rounded-box p-3 flex">
-          <span className="pr-8">
-            <input type="checkbox" checked={true} className="checkbox" />
-          </span>
-          <span className="flex-grow line-through">
-            Me faire défoncer à LoL
-          </span>
-          <div>
-            <button className="btn btn-error btn-outline btn-xs">X</button>
-          </div>
-        </div>
+        <TodoItem description="Acheter des oranges" checked={false} />
+        <TodoItem description="Courir avec le fraté" checked={true} />
+        <TodoItem description="Me faire défoncer à LoL" checked={true} />
       </div>
     </>
   );
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,23 @@
+type Props = {
+  description: string;
+  checked: boolean;
+};
+
+function TodoItem({ description, checked }: Props) {
+  return (
+    <div className="bg-indigo-700 w-full m-5 rounded-box p-3 flex">
+      <span className="pr-8">
+        <input type="checkbox" className="checkbox" checked={checked} />
+      </span>
+      {/* Utilisation de l'opérateur logique && pour ajouter dynamique la classe quand checked vaut true */}
+      <span className={`flex-grow ${checked && "line-through"}`}>
+        {description}
+      </span>
+      <div>
+        <button className="btn btn-error btn-outline btn-xs">X</button>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;
