f65ac45be23ba29ef877d63bcd482babcb14f561
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-03-13 14:21:05 +0100

    UPDATE (TodoApp) Create a list of toto items and iterate through to display <TodoItem> component

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,7 +1,19 @@
 import AddTodo from "./AddTodo";
 import TodoItem from "./TodoItem";
 
+type TodoItem = {
+  id: string;
+  description: string;
+  checked: boolean;
+};
+
 export function TodoApp() {
+  const todos: TodoItem[] = [
+    { id: "1", description: "Acheter des oranges", checked: false },
+    { id: "2", description: "Courir avec le fraté", checked: true },
+    { id: "3", description: "Me faire défoncer à LoL", checked: true },
+  ];
+
   return (
     <>
       <div className="flex">
@@ -9,9 +21,13 @@ export function TodoApp() {
       </div>
 
       <div className="my-5 flex-column gap-5 w-full text-left">
-        <TodoItem description="Acheter des oranges" checked={false} />
-        <TodoItem description="Courir avec le fraté" checked={true} />
-        <TodoItem description="Me faire défoncer à LoL" checked={true} />
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            description={todo.description}
+            checked={todo.checked}
+          />
+        ))}
       </div>
     </>
   );
