1d1050dc6d80f74d911dbcba81a643318a9c6921
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-03-13 14:06:28 +0100

    DELETE ts type

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,67 +1,49 @@
-type TodoItem = {
-    id: number,
-    description: string
-    done: boolean
-}
-
 export function TodoApp() {
-    return (
-        <>
-            <div className="flex">
+  return (
+    <>
+      <div className="flex">
+        <label className="input input-bordered flex items-center gap-2">
+          <input type="text" className="grow" placeholder="Ajouter une tâche" />
+        </label>
 
-                <label className="input input-bordered flex items-center gap-2">
-                    <input type="text" className="grow" placeholder="Ajouter une tâche" />
-                </label>
+        <button className="btn btn-primary">+</button>
+      </div>
 
-                <button className="btn btn-primary">+</button>
+      <div className="my-5 flex-column gap-5 w-full text-left">
+        {/* TODO ITEM version normal */}
+        <div className="bg-indigo-700 w-full m-5 rounded-box p-3 flex">
+          <span className="pr-8">
+            <input type="checkbox" className="checkbox" />
+          </span>
+          <span className="flex-grow">Acheter des oranges</span>
+          <div>
+            <button className="btn btn-error btn-outline btn-xs">X</button>
+          </div>
+        </div>
 
-            </div>
+        {/* TODO Item version cochée */}
+        <div className="bg-indigo-900 w-full m-5 rounded-box p-3 flex">
+          <span className="pr-8">
+            <input type="checkbox" checked={true} className="checkbox" />
+          </span>
+          <span className="flex-grow line-through">Courir avec le fraté</span>
+          <div>
+            <button className="btn btn-error btn-outline btn-xs">X</button>
+          </div>
+        </div>
 
-            <div className="my-5 flex-column gap-5 w-full text-left">
-                {/* TODO ITEM version normal */}
-                <div className="bg-indigo-700 w-full m-5 rounded-box p-3 flex">
-                    <span className="pr-8">
-                        <input type="checkbox" className="checkbox" />
-                    </span>
-                    <span className="flex-grow">
-                        Acheter des oranges
-                    </span>
-                    <div>
-                        <button className="btn btn-error btn-outline btn-xs">
-                            X
-                        </button>
-                    </div>
-                </div>
-
-                {/* TODO Item version cochée */}
-                <div className="bg-indigo-900 w-full m-5 rounded-box p-3 flex">
-                    <span className="pr-8">
-                        <input type="checkbox" checked={true} className="checkbox" />
-                    </span>
-                    <span className="flex-grow line-through">
-                        Courir avec le fraté
-                    </span>
-                    <div>
-                        <button className="btn btn-error btn-outline btn-xs">
-                            X
-                        </button>
-                    </div>
-                </div>
-
-                <div className="bg-indigo-900 w-full m-5 rounded-box p-3 flex">
-                    <span className="pr-8">
-                        <input type="checkbox" checked={true} className="checkbox" />
-                    </span>
-                    <span className="flex-grow line-through">
-                        Me faire défoncer à LoL
-                    </span>
-                    <div>
-                        <button className="btn btn-error btn-outline btn-xs">
-                            X
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+        <div className="bg-indigo-900 w-full m-5 rounded-box p-3 flex">
+          <span className="pr-8">
+            <input type="checkbox" checked={true} className="checkbox" />
+          </span>
+          <span className="flex-grow line-through">
+            Me faire défoncer à LoL
+          </span>
+          <div>
+            <button className="btn btn-error btn-outline btn-xs">X</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
